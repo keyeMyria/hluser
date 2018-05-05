55d2cdad2bdf1dc3ede28628d6ebfc4812eaea24
Francisco Chim <fcochim@icloud.com>
2018-05-05 04:02:41 -0500

    Mapbox Android check

--- App.js
+++ App.js
@@ -13,6 +13,9 @@ import {
   TouchableOpacity,
 } from 'react-native';
 import firebase from 'react-native-firebase';
+import Mapbox from '@mapbox/react-native-mapbox-gl';
+
+Mapbox.setAccessToken('pk.eyJ1IjoiZmNvY2hpbXZlcmEiLCJhIjoiY2pnNHhpc3RkMWZlcDMzeGYzemI5dXhxcCJ9.UZ_9JEKLgZGM-89Rek1MFA');
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -32,22 +35,37 @@ export default class App extends Component<Props> {
   });
   }
 
+  renderAnnotations () {
+    return (
+      <Mapbox.PointAnnotation
+        key='pointAnnotation'
+        id='pointAnnotation'
+        coordinate={[11.254, 43.772]}>
+
+        <View style={styles.annotationContainer}>
+          <View style={styles.annotationFill} />
+        </View>
+        <Mapbox.Callout title='Look! An annotation!' />
+      </Mapbox.PointAnnotation>
+    )
+  }
+
 
 
   render() {
     return (
       <View style={styles.container}>
-        <TouchableOpacity onPress={() => this.login()}>
-          <Text style={styles.welcome}>
-            Welcome to React Native!
-          </Text>
-        </TouchableOpacity>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Mapbox.MapView
+            styleURL={Mapbox.StyleURL.Street}
+            zoomLevel={15}
+            centerCoordinate={[11.256, 43.770]}
+            style={styles.container}
+            showUserLocation={true}>
+            {this.renderAnnotations()}
+        </Mapbox.MapView>
+        <View>
+            <Text> Si esta cargando</Text>
+        </View>
       </View>
     );
   }
@@ -56,9 +74,6 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
   welcome: {
     fontSize: 20,
@@ -70,4 +85,19 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  annotationContainer: {
+    width: 30,
+    height: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    borderRadius: 15,
+  },
+  annotationFill: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    backgroundColor: 'orange',
+    transform: [{ scale: 0.6 }],
+  }
 });
